1f519a44318c8537c451e4199e79d417ea08c9d0
madhukar eppalapelly <madhukar.eppalapelly@gmail.com>
2023-03-12 21:39:32 +0530

    body parser

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const path = require("path")
 const bodyParser = require("body-parser");
  app.use(bodyParser.json());
  
-const expressErrorHandler=require("express-async-handler")
+const userApi=require("./apis/user-api")
+const itemsApi=require("./apis/itemsdata")
+ 
 
 
 const cors = require('cors')
@@ -23,22 +25,10 @@ app.get("/", (req, res) => {
 //connecting front end to backend
 
 //importing
-const userApi=require("./apis/user-api")
-const itemsApi=require("./apis/itemsdata")
-
-
 app.use("/user",userApi)
 app.use("/items",itemsApi)
 
 
-
-app.get('/addtocart',expressErrorHandler(async(req,res,next)=>{
-    console.log("backend called")
-    let arr=[1,2,34]
-    res.send({message:arr})
-}))
-//connect angular app with express server
-app.use(exp.static(path.join(__dirname, './dist/delish/')))
 
 
 app.use((req,res)=>{
@@ -52,3 +42,15 @@ app.use((err,req,res,next)=>{
 
 const port=5000
 app.listen(port,()=>console.log(`Server can hear you on ${port}....`))
+
+
+
+/*
+app.get('/addtocart',expressErrorHandler(async(req,res,next)=>{
+    console.log("backend called")
+    let arr=[1,2,34]
+    res.send({message:arr})
+}))
+//connect angular app with express server
+app.use(exp.static(path.join(__dirname, './dist/delish/')))
+*/
